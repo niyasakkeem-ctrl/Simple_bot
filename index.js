a0a5a52e4fb74db799b25d98b372592a6df57c72
niyasakkeem-ctrl <niyasakkeem@gmail.com>
2026-05-22 23:12:07 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+
+  if (message.content === "hi") {
+    message.reply("Hello 👋");
+  }
+
+  if (message.content === "ping") {
+    message.reply("pong 🏓");
+  }
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
